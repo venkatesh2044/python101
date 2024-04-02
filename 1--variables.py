ccc10684771e47418a85ea3dce376aa4a0634e0a
VENKATESH THUMMALAPENTA <venky.artificialintelligence@gmail.com>
2024-04-02 16:42:57 +0000

    implemented day-3

---
+++ 1--variables.py
@@ -0,0 +1,21 @@
+print('Welcome to spatial technologies')
+
+print ('spat+ial')
+
+print(4//2)
+
+number=1
+print(type(number))
+number = float(number)
+print(type(number))
+number=int(number)
+print(type(1.1))
+import math
+
+number = 5.7
+
+result_floor = math.floor(number)
+result_ceil = math.ceil(number)
+
+print("Floor of", number, "is:", result_floor)
+print("Ceiling of", number, "is:", result_ceil)
